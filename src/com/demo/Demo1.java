16f18a34ff522ff86f7095ba480a811da5f85e87
Mehul <patel.mehul45@yahoo.com>
2019-05-20 12:29:07 -0400

    First Commit on GitDemoV1

---
+++ src/com/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes") {
+			System.out.println("Feed a food");
+		}
+	}
+}
