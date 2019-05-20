2fb1c86ff690ff9a32f2630e7bbff8d67ded1512
Mehul <patel.mehul45@yahoo.com>
2019-05-20 12:41:44 -0400

    Second commit for add demo2 class

---
+++ src/com/demo/Demo2.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes" || hungry == "y") {
+			System.out.println("Feed a food");
+		}
+		else {
+			System.out.println("No food needed");
+		}
+	}
+}
